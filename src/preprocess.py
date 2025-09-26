4fe0cdc2166b1ae826539e9aeaecec8ddbd4ecb0
N-Shimoda <naoki.shimoda@outlook.jp>
2025-09-26 12:37:36 +0900

    updated ans label with blank

--- src/preprocess.py
+++ src/preprocess.py
@@ -12,7 +12,7 @@ def add_graph_column(example, k: int = 4):
     edges = extract_edges_from_text(text)
 
     example["prompt"] = example["task_description"]
-    example["completion"] = example["answer"].strip()
+    example["completion"] = example["answer"].strip() + " "
     example["graph"] = create_pyg_dict(nodes, edges, k=k)  # k: dimension of LPE
     return example
 
