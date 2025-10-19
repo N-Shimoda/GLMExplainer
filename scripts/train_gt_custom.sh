ad066f8d672d5fb86e0c124f2d8e4ee51a8923fa
N-Shimoda <naoki.shimoda@outlook.jp>
2025-10-19 15:50:15 +0900

    created train_gt_custom

--- scripts/train_gt_custom.sh
+++ scripts/train_gt_custom.sh
@@ -20,25 +20,24 @@ log() {
 }
 
 subsets=(
-	node_count
+	# node_count
 	edge_count
-	cycle_check
-	triangle_counting
+	# cycle_check
+	# triangle_counting
 )
 
 for subset in "${subsets[@]}"; do
 	cmd=(
 		torchrun --nproc_per_node=2 train.py --subset "${subset}"
+		--use-custom-dataset
 		--base-model "Qwen/Qwen3-4B-Base"
 		--gnn-type "GCN"
 		--num-graph-tokens 4 --node-feat-dim 8 --pos-emb-dim 8
 		--gnn-hidden-dim 256 --gnn-out-dim 512 --num-gnn-layers 4
 		--num-proj-layers 1
-		--epochs 12
-		# --optim "lion" --lr 0.01
-		# --lr-scheduler-type "linear" --warmup-ratio 0.05
-		--optim "adamw" --lr 0.0075 --weight-decay 0.01
-		--lr-scheduler-type "cosine" --warmup-ratio 0.05
+		--epochs 3
+		--optim "lion" --lr 0.01
+		--lr-scheduler-type "linear" --warmup-ratio 0.05
 		--do-eval --wandb
 	)
 	log "[START] ${cmd[*]}"
