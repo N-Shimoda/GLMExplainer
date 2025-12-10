26ea5b060be50970925eab2838d019926ce63800
N-Shimoda <naoki.shimoda@outlook.jp>
2025-12-10 18:31:46 +0900

    ready for MotifQA GCN training

--- scripts/train_gt_all.sh
+++ scripts/train_gt_all.sh
@@ -24,15 +24,16 @@ graphqa_subsets=(
 	# edge_count
 	# cycle_check
 	# triangle_counting
-	node_degree
-	reachability
-	edge_existence
+	# node_degree
+	# reachability
+	# edge_existence
 )
 
 motifqa_subsets=(
-	# ba_shapes
-	# tree_cycle
-	# tree_grid
+	ba_shapes
+	tree_cycle
+	tree_grid
+	ba_two_motifs
 )
 
 gnns=(
@@ -60,7 +61,8 @@ run_train_loop() {
 		# --lr-scheduler-type "linear" --warmup-ratio 0.05
 		--optim "adamw" --lr 0.0075 --weight-decay 0.01
 		--lr-scheduler-type "cosine" --warmup-ratio 0.05
-		--do-eval --wandb
+		--do-eval
+		--wandb
 	)
 	log "[START] ${cmd[*]}"
 	start_ts=$(date +%s)
