6b83ad394fd3a3376e73ee1fc7af7f4fad5f8e8e
N-Shimoda <naoki.shimoda@outlook.jp>
2026-01-07 17:26:49 +0900

    renamed scripts

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 pg = st.navigation(
     [
-        st.Page("pages/table_viewer.py", title="Analysis", icon="📊"),
-        st.Page("pages/graph_viewer.py", title="Graph Viewer", icon="🔍"),
+        st.Page("pages/Analysis.py", title="Analysis", icon="📊"),
+        st.Page("pages/GraphViewer.py", title="Graph Viewer", icon="🔍"),
     ]
 )
 pg.run()
