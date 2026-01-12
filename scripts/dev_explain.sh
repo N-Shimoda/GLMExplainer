1b147c02c323ef7917813f19437553654694712d
N-Shimoda <naoki.shimoda@outlook.jp>
2026-01-12 17:47:03 +0900

    updated dev_explain.sh for exploring threshold of ba_shapes

--- scripts/dev_explain.sh
+++ scripts/dev_explain.sh
@@ -1,9 +1,11 @@
-torchrun --nproc_per_node=2 explain.py \
-	--dataset MotifQA --subset ba_shapes \
-	--model-path masters/ba_shapes \
-	--target-pos-samples \
-	--num-trials 4 --num-samples 2 \
-	--llr-threshold 3.0 --baseline-graph "complete" \
-	--num-gen-trials 10 \
-	--epochs 200 --lr 0.01 \
-	--edge-size 96 --edge-ent 1.0
+for thresh in 0.0 1.5 3.0 4.5; do
+	torchrun --nproc_per_node=2 explain.py \
+		--dataset MotifQA --subset ba_shapes \
+		--model-path masters/ba_shapes \
+		--target-pos-samples \
+		--num-trials 4 --num-samples 2 \
+		--llr-threshold $thresh --baseline-graph "complete" \
+		--num-gen-trials 10 \
+		--epochs 200 --lr 0.01 \
+		--edge-size 96 --edge-ent 1.0
+done
