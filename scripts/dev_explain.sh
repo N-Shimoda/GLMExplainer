1a827267cac7db86585b480c0f1aded0f6d9a901
N-Shimoda <naoki.shimoda@outlook.jp>
2026-01-12 14:45:33 +0900

    updated dev script

--- scripts/dev_explain.sh
+++ scripts/dev_explain.sh
@@ -2,6 +2,6 @@ torchrun --nproc_per_node=2 explain.py \
 	--dataset MotifQA --subset ba_shapes \
 	--model-path outputs/ba_shapes \
 	--target-pos-samples \
-	--num-trials 2 --num-samples 4 \
+	--num-trials 10 --num-samples 2 \
 	--epochs 200 --lr 0.01 \
 	--edge-size 0.005 --edge-ent 1.0
