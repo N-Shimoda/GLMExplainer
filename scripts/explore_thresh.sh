20d753ecd4482c028385398e77282d5d3fd6c256
Naoki Shimoda <naoki.shimoda@outlook.jp>
2026-01-13 20:40:32 +0900

    added lr to exploring loop

--- scripts/explore_thresh.sh
+++ scripts/explore_thresh.sh
@@ -1,13 +1,15 @@
-for edge_size in 1e-4 1e-3 1e-2 1e-1 1 10 100; do
-	for thresh in 3.0 4.0; do
-		torchrun --nproc_per_node=2 explain.py \
-			--dataset MotifQA --subset ba_shapes \
-			--model-path masters/ba_shapes \
-			--target-pos-samples --num-samples 20 \
-			--num-trials 5 \
-			--epochs 200 --lr 0.01 \
-			--edge-size $edge_size --edge-ent 1.0 \
-			--llr-threshold $thresh --baseline-graph "complete" \
-			--wandb --tags thresh_search mini
+for edge_size in 1e-3 1e-2 1e-1 1 10 100 1000; do
+	for thresh in 1.0 2.0 3.0; do
+		for lr in 0.1 1; do
+			torchrun --nproc_per_node=2 explain.py \
+				--dataset MotifQA --subset ba_shapes \
+				--model-path masters/ba_shapes \
+				--target-pos-samples --num-samples 50 \
+				--num-trials 5 \
+				--epochs 200 --lr $lr \
+				--edge-size $edge_size --edge-ent 1.0 \
+				--llr-threshold $thresh --baseline-graph "complete" \
+				--wandb --tags thresh_search small
+		done
 	done
 done
