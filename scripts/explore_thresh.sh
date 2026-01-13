142481e6bf7a87a46c97f41f4a6c1bd52f76d520
N-Shimoda <naoki.shimoda@outlook.jp>
2026-01-13 18:21:11 +0900

    explore thresh=4.0

--- scripts/explore_thresh.sh
+++ scripts/explore_thresh.sh
@@ -1,5 +1,5 @@
-for thresh in 0.0 2.0; do
-	for edge_size in 1e-4 1e-3 3e-3 1e-2 1e-1; do
+for edge_size in 1e-4 1e-3 1e-2 1e-1 1 10 100; do
+	for thresh in 4.0; do
 		torchrun --nproc_per_node=2 explain.py \
 			--dataset MotifQA --subset ba_shapes \
 			--model-path masters/ba_shapes \
