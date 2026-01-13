17e39ea84a0f1736d170f2ecb08a5ea293c3de70
N-Shimoda <naoki.shimoda@outlook.jp>
2026-01-13 12:11:24 +0900

    created explore script

--- scripts/explore_thresh.sh
+++ scripts/explore_thresh.sh
@@ -1,9 +1,9 @@
-for thresh in 0.0 2.0 4.0; do
-	for edge_size in 1.0; do
+for thresh in 0.0 2.0; do
+	for edge_size in 1e-4 1e-3 3e-3 1e-2 1e-1; do
 		torchrun --nproc_per_node=2 explain.py \
 			--dataset MotifQA --subset ba_shapes \
 			--model-path masters/ba_shapes \
-			--target-pos-samples --num-samples 25 \
+			--target-pos-samples --num-samples 20 \
 			--num-trials 5 \
 			--epochs 200 --lr 0.01 \
 			--edge-size $edge_size --edge-ent 1.0 \
