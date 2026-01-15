cbbd2bb466e7161a399291e8a0df02b5ccbb7bb0
N-Shimoda <naoki.shimoda@outlook.jp>
2026-01-15 17:31:14 +0900

    updated script for testing shortest_path training

--- scripts/train_gt_all.sh
+++ scripts/train_gt_all.sh
@@ -30,10 +30,11 @@ graphqa_subsets=(
 )
 
 motifqa_subsets=(
-	ba_shapes
-	tree_cycle
-	tree_grid
-	ba_two_motifs
+	# ba_shapes
+	# tree_cycle
+	# tree_grid
+	# ba_two_motifs
+	shortest_path
 )
 
 gnns=(
