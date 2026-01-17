b510526e6659c4bbcaa4ec94176b9b61ee49a121
N-Shimoda <naoki.shimoda@outlook.jp>
2026-01-17 18:46:52 +0900

    updated scripts/exp

--- scripts/exp/gnn_size.sh
+++ scripts/exp/gnn_size.sh
@@ -1,4 +1,4 @@
-for gnn_dim in 16 32 64 128; do
+for gnn_dim in 16 32 64 128 256; do
 	torchrun --nproc_per_node=2 train.py \
 		--dataset MotifQA --subset ba_shapes \
 		--lpe-dim 8 --use-degree-emb \
@@ -7,7 +7,7 @@ for gnn_dim in 16 32 64 128; do
 		--gnn-hidden-dim "${gnn_dim}" --gnn-out-dim "$((gnn_dim * 2))" \
 		--num-gnn-layers 3 \
 		--num-graph-tokens 4 \
-		--epochs 12 \
+		--epochs 24 --no-save \
 		--optim adamw \
 		--lr 0.0075 --weight-decay 0.01 \
 		--lr-scheduler-type cosine --warmup-ratio 0.05 \
