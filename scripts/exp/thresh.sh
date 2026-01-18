d312346b5be83074c42bbda261843c785250e889
N-Shimoda <naoki.shimoda@outlook.jp>
2026-01-18 23:32:15 +0900

    updated script with llr_thresh=0.0

--- scripts/exp/thresh.sh
+++ scripts/exp/thresh.sh
@@ -4,26 +4,18 @@ set -euo pipefail
 
 for subset in ba_shapes tree_cycle tree_grid ba_two_motifs shortest_path; do
 	for edge_size in 1e-2 1e-1 1 10 100; do
-		# Without thresholding
-		cmd_v1=(
-			torchrun --nproc_per_node=2 explain.py
-			--dataset MotifQA --subset "$subset"
-			--model-path masters/"$subset"
-			--target-pos-samples --num-samples 50
-			--num-trials 5
-			--epochs 200 --lr 3.0
-			--edge-size "$edge_size" --edge-ent 1.0
-			--wandb
-			--tags thresh_search small master
-		)
-		"${cmd_v1[@]}"
-
-		# With thresholding
-		cmd_v2=(
-			"${cmd_v1[@]}"
-			--llr-threshold 1.0
-			--baseline-graph "complete"
-		)
-		"${cmd_v2[@]}"
+		for thresh in 0.0 1.0; do
+			torchrun --nproc_per_node=2 explain.py \
+				--dataset MotifQA --subset "$subset" \
+				--model-path masters/"$subset" \
+				--target-pos-samples --num-samples 50 \
+				--num-trials 5 \
+				--epochs 200 --lr 3.0 \
+				--edge-size "$edge_size" --edge-ent 1.0 \
+				--llr-threshold "$thresh" \
+				--baseline-graph "complete" \
+				--wandb \
+				--tags thresh_search small master
+		done
 	done
 done
