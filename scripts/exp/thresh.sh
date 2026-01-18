f6bb4e45b33a6fc7ac93eed3b7a62c33ffc6a288
N-Shimoda <naoki.shimoda@outlook.jp>
2026-01-18 18:41:55 +0900

    updated script for explore thresh for all subsets

--- scripts/exp/thresh.sh
+++ scripts/exp/thresh.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-for subset in tree_grid; do
-	for lr in 0.1 1.0 3.0; do
+for subset in ba_shapes tree_cycle tree_grid ba_two_motifs shortest_path; do
+	for lr in 1.0 3.0; do
 		for edge_size in 1e-3 1e-2 1e-1 1 10 100; do
-			for thresh in 0.0 1.0; do
-				torchrun --nproc_per_node=2 explain.py \
-					--dataset MotifQA --subset $subset \
-					--model-path masters/"$subset" \
-					--target-pos-samples --num-samples 50 \
-					--num-trials 5 \
-					--epochs 200 --lr $lr \
-					--edge-size $edge_size --edge-ent 1.0 \
-					--llr-threshold $thresh --baseline-graph "complete" \
-					--wandb --tags thresh_search small
-			done
+			# Without thresholding
+			wo_cmd=(
+				torchrun --nproc_per_node=2 explain.py
+				--dataset MotifQA --subset "$subset"
+				--model-path masters/"$subset"
+				--target-pos-samples --num-samples 50
+				--num-trials 5
+				--epochs 200 --lr "$lr"
+				--edge-size "$edge_size" --edge-ent 1.0
+				--wandb --tags thresh_search small
+			)
+			"${wo_cmd[@]}"
+
+			# With thresholding
+			w_cmd=(
+				"${wo_cmd[@]}"
+				--llr-threshold 1.0
+				--baseline-graph "complete"
+			)
+			"${w_cmd[@]}"
 		done
 	done
 done
