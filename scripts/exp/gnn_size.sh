e2bba8a4b6b2b8147dec48a5d0f71c10d20f6f6b
N-Shimoda <naoki.shimoda@outlook.jp>
2026-01-18 13:17:31 +0900

    test for gnn_dim=512

--- scripts/exp/gnn_size.sh
+++ scripts/exp/gnn_size.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-for epochs in 12 24; do
-	for gnn_dim in 8 16 32 64 128 256; do
-		torchrun --nproc_per_node=2 train.py \
-			--dataset MotifQA --subset shortest_path \
-			--lpe-dim 8 --use-degree-emb \
-			--pos-emb-dim 8 \
-			--gnn-type GCN \
-			--gnn-hidden-dim $gnn_dim --gnn-out-dim $((gnn_dim * 2)) \
-			--num-gnn-layers 3 \
-			--num-graph-tokens 4 \
-			--epochs $epochs \
-			--optim adamw \
-			--lr 0.0075 --weight-decay 0.01 \
-			--lr-scheduler-type cosine --warmup-ratio 0.05 \
-			--do-eval --no-save \
-			--wandb --tags hidden_dim
-	done
-done
-
-for subset in ba_shapes tree_grid tree_cycle ba_two_motifs; do
+for subset in ba_shapes tree_grid tree_cycle ba_two_motifs shortest_path; do
 	for epochs in 12 24; do
-		for gnn_dim in 8; do
+		for gnn_dim in 512; do
 			torchrun --nproc_per_node=2 train.py \
 				--dataset MotifQA --subset $subset \
 				--lpe-dim 8 --use-degree-emb \
