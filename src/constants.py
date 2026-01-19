d72bbe5e497bd8fafbb9e6d6a3a22f606a14c1b9
N-Shimoda <naoki.shimoda@outlook.jp>
2026-01-19 14:01:43 +0900

    defined subset types as constants

--- src/constants.py
+++ src/constants.py
@@ -8,3 +8,19 @@ GRAPHQA_SUBSETS = [
     "edge_existence",
 ]
 MOTIFQA_SUBSETS = ["ba_shapes", "tree_cycle", "tree_grid", "ba_two_motifs", "shortest_path"]
+
+NUMERIC_SUBSETS = {
+    "node_count": int,
+    "edge_count": int,
+    "triangle_counting": int,
+    "node_degree": int,
+}
+CLASSIFICATION_SUBSETS = {
+    "cycle_check": {"yes", "no"},
+    "reachability": {"yes", "no"},
+    "edge_existence": {"yes", "no"},
+    "ba_shapes": {"yes", "no"},
+    "tree_cycle": {"yes", "no"},
+    "tree_grid": {"yes", "no"},
+    "ba_two_motifs": {"house", "cycle"},
+}
