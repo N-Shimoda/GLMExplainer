b02b143b2df3c81003945f3c4b9d971912397cb4
N-Shimoda <naoki.shimoda@outlook.jp>
2026-01-19 13:57:34 +0900

    testing without degree emb

--- scripts/dev/explain.sh
+++ scripts/dev/explain.sh
@@ -4,8 +4,8 @@ torchrun --nproc_per_node=2 explain.py \
 	--dataset MotifQA --subset ba_shapes \
 	--model-path masters/ba_shapes \
 	--target-pos-samples \
-	--num-samples 10 --num-trials 2 \
-	--llr-threshold 2.0 --baseline-graph "complete" \
-	--epochs 200 --lr 0.01 \
+	--num-samples 50 --num-trials 5 \
+	--llr-threshold 3.0 \
+	--epochs 200 --lr 3.0 \
 	--edge-size 96 --edge-ent 1.0 \
 	--wandb --tags dev
--- scripts/dev/train.sh
+++ scripts/dev/train.sh
@@ -2,16 +2,15 @@
 
 torchrun --nproc_per_node=2 train.py \
 	--dataset MotifQA --subset ba_shapes \
-	--lpe-dim 8 --use-degree-emb \
+	--lpe-dim 8 \
 	--pos-emb-dim 8 \
 	--gnn-type GCN \
 	--gnn-hidden-dim 16 --gnn-out-dim 32 \
 	--num-gnn-layers 3 \
 	--num-graph-tokens 4 \
-	--epochs 2 \
+	--epochs 24 \
 	--optim adamw \
 	--lr 0.0075 --weight-decay 0.01 \
 	--lr-scheduler-type cosine --warmup-ratio 0.05 \
 	--do-eval \
-	--no-save \
 	--wandb --tags dev
