5d47a1f259b3386b599b42d4fa0d9cd6977fe0e2
N-Shimoda <naoki.shimoda@outlook.jp>
2026-01-31 17:24:41 +0900

    debugged script

--- scripts/train/gnn_layers.sh
+++ scripts/train/gnn_layers.sh
@@ -10,13 +10,14 @@ for gnn in "${gnn_types[@]}"; do
 	for subset in "${subsets[@]}"; do
 		for num_layer in 3 4 5; do
 			for graph_pooling in "${graph_poolings[@]}"; do
+				read -r -a pooling_args <<<"${graph_pooling}"
 				torchrun --nproc_per_node=2 train.py \
 					--dataset "MotifQA" --subset "${subset}" \
 					--gnn-type "${gnn}" \
 					--lpe-dim 8 --pos-emb-dim 8 \
 					--gnn-hidden-dim 256 --gnn-out-dim 512 \
 					--num-gnn-layers "${num_layer}" \
-					--graph-pooling "${graph_pooling}" \
+					--graph-pooling "${pooling_args[@]}" \
 					--num-graph-tokens 4 \
 					--epochs 24 \
 					--optim "adamw" --lr 0.0075 --weight-decay 0.01 \
