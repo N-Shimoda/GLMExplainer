668726f2150c322d1af1fe32438e5ebe07631cd9
N-Shimoda <naoki.shimoda@outlook.jp>
2026-02-02 16:35:29 +0900

    updated for multitask learning

--- scripts/dev/multitask.sh
+++ scripts/dev/multitask.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 torchrun --nproc_per_node=2 train.py \
-	--dataset MotifQA --subset ba_shapes tree_cycle \
+	--dataset MotifQA \
+	--subset ba_shapes tree_cycle tree_grid tree_grid_v2 ba_two_motifs \
 	--lpe-dim 8 \
 	--pos-emb-dim 8 \
 	--gnn-type GAT \
 	--gnn-hidden-dim 32 --gnn-out-dim 32 \
 	--num-gnn-layers 5 \
 	--num-graph-tokens 4 \
-	--epochs 2 \
+	--epochs 24 \
 	--optim adamw \
 	--lr 0.0075 --weight-decay 0.01 \
 	--lr-scheduler-type cosine --warmup-ratio 0.05 \
