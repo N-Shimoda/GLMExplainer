8c4a909565dea05a94cdd60b5cb7bb2aca5e6489
N-Shimoda <naoki.shimoda@outlook.jp>
2026-02-03 18:25:19 +0900

    ready to explore GIN & GraphTransformer

--- scripts/train/multitask.sh
+++ scripts/train/multitask.sh
@@ -2,24 +2,49 @@
 
 set -euo pipefail
 
-gnn="GIN"
+# GIN
 hidden_dim=64
+for num_gnn_layers in 2 3 5; do
+	for graph_pooling in "sum" "mean"; do
+		torchrun --nproc_per_node=2 train.py \
+			--dataset MotifQA \
+			--subset ba_shapes ba_two_motifs tree_cycle tree_grid_v2 \
+			--lpe-dim 8 --pos-emb-dim 8 \
+			--gnn-type "GIN" \
+			--gnn-hidden-dim $hidden_dim --gnn-out-dim $hidden_dim \
+			--num-gnn-layers $num_gnn_layers \
+			--graph-pooling "$graph_pooling" \
+			--num-proj-layers 2 \
+			--num-graph-tokens 4 \
+			--epochs 32 \
+			--optim adamw \
+			--lr 5e-3 --weight-decay 1e-2 \
+			--lr-scheduler-type cosine --warmup-ratio 0.05 \
+			--do-eval \
+			--wandb --tags multitask
+	done
+done
 
-for graph_pooling in "sum" "mean"; do
-	torchrun --nproc_per_node=2 train.py \
-		--dataset MotifQA \
-		--subset ba_shapes ba_two_motifs tree_cycle tree_grid_v2 \
-		--lpe-dim 8 --pos-emb-dim 8 \
-		--gnn-type $gnn \
-		--gnn-hidden-dim $hidden_dim --gnn-out-dim $hidden_dim \
-		--num-gnn-layers 5 \
-		--graph-pooling "$graph_pooling" \
-		--num-proj-layers 2 \
-		--num-graph-tokens 4 \
-		--epochs 32 \
-		--optim adamw \
-		--lr 5e-3 --weight-decay 1e-2 \
-		--lr-scheduler-type cosine --warmup-ratio 0.05 \
-		--do-eval \
-		--wandb --tags multitask
+# GraphTransformer
+for hidden_dim in 23 64 128; do
+	for num_gnn_layers in 3 5; do
+		for graph_pooling in "sum" "mean"; do
+			torchrun --nproc_per_node=2 train.py \
+				--dataset MotifQA \
+				--subset ba_shapes ba_two_motifs tree_cycle tree_grid_v2 \
+				--lpe-dim 8 --pos-emb-dim 8 \
+				--gnn-type "GraphTransformer" \
+				--gnn-hidden-dim $hidden_dim --gnn-out-dim $hidden_dim \
+				--num-gnn-layers $num_gnn_layers \
+				--graph-pooling "$graph_pooling" \
+				--num-proj-layers 2 \
+				--num-graph-tokens 4 \
+				--epochs 32 \
+				--optim adamw \
+				--lr 5e-3 --weight-decay 1e-2 \
+				--lr-scheduler-type cosine --warmup-ratio 0.05 \
+				--do-eval \
+				--wandb --tags multitask
+		done
+	done
 done
