b83c12a28356060d9151e63d469243b22e9d395b
N-Shimoda <naoki.shimoda@outlook.jp>
2026-02-05 17:25:45 +0900

    created explain dir

---
+++ scripts/explain/edge_size.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+subsets=(ba_shapes tree_cycle tree_grid_v2 ba_two_motifs)
+jaccard_k_values=(6 6 7 5)
+edge_sizes=(1e-1 3e-1 1e0 3e0 1e1 3e1 1e2 3e2 1e3)
+
+for i in "${!subsets[@]}"; do
+	for edge_size in "${edge_sizes[@]}"; do
+		torchrun --nproc_per_node=2 explain.py \
+			--dataset MotifQA \
+			--subset "${subsets[$i]}" --split validation \
+			--model-path masters/multitask \
+			--target-pos-samples \
+			--num-trials 5 \
+			--epoch 200 --lr 0.3 \
+			--edge-size "${edge_size}" --edge-ent 1.0 \
+			--jaccard-k "${jaccard_k_values[$i]}" \
+			--wandb --tags edge_size
+	done
+done
